fba9dc7d9a3d96522e741e4216d3c2ffe9f58f97
Munechika Sumikawa <sumikawa@sumikawa.jp>
2011-08-26 19:27:03 +0900

    current

---
+++ devquiz.pl
@@ -0,0 +1,213 @@
+#!/usr/bin/perl
+
+#$current = "12=E4D9HIF8=GN576LOABMTPKQSR0J";
+#$good    = "12=3456789A=BCDEFGHIJKLMNOPQR0";
+
+$current = "168452=30";
+$good    = "123456=70";
+
+$num = 0;
+$min = 100000;
+my %done;
+$path = "";
+$| = 1;
+
+$width = 3;
+$height = 3;
+
+sub r {
+    if (index($_[0], "0") % $width != ($width - 1)) {
+	if (not $_[0] =~ m/0=/) {
+	    $reg = '.' x ($width - 2);
+	    $_[0] =~ s/0($reg)/$1 0/;
+	    $_[0] =~ s/ //;
+	    return 0;
+	}
+    }
+    return 1;
+}
+
+sub l {
+    if (index($_[0], "0") % $width != 0) {
+	if (not $_[0] =~ m/=0/) {
+	    $reg = '.' x ($width - 2);
+	    $_[0] =~ s/($reg)0/0$1/;
+	    return 0;
+	}
+    }
+    return 1;
+}
+
+sub u {
+    if (index($_[0], "0") > $width) {
+	$reg = '.' x ($width - 1);
+	if (not $_[0] =~ m/=($reg)0/) {
+	    $_[0] =~ s/(.)($reg)0/0$2$1/;
+	    return 0;
+	}
+    }
+    return 1;
+}
+
+sub d {
+    if (length($_[0]) - index($_[0], "0") > $width) {
+	$reg = '.' x ($width - 1);
+	if (not $_[0] =~ m/0($reg)=/) {
+	    $_[0] =~ s/0($reg)(.)/$2$1 0/;
+	    $_[0] =~ s/ //;
+	    return 0;
+	}
+    }
+    return 1;
+}
+
+sub srch {
+    # $_[0] = $current;
+    # $_[1] = $num;
+    # $_[2] = $path;
+    
+    # 終了したか
+    #   YES
+    #     探索回数を確認して、最短なら更新
+    #     ret
+    # L動くか
+    #   YES
+    #     動いた先は過去にあった局面か
+    #       YES
+    #         探索終了
+    #       NO
+    #         再帰呼出
+    #   NO
+    #         探索終了
+    # R動くか
+    # U動くか
+    # D動くか
+    print $_[0] . ": ";
+    print $_[2] . "\n";
+    if ($_[0] eq $good) {
+	print "good = $_[0]\n";
+	if ($_[1] < $min) {
+	    $minpath = $_[2];	
+	    print "minpath = $minpath\n";
+	}
+	return;
+    }
+    if (l($current) == 0) {
+	if (exists($done{$_[0]})) {
+	    return;
+	} else {
+	    $done{$_[0]} = 1;
+	    $_[1]++;
+	    $_[2] = $_[2] . "L";
+	    srch($_[0], $_[1], $_[2])
+	}
+    } else {
+	return;
+    }
+    if (r($current) == 0) {
+	if (exists($done{$_[0]})) {
+	    return;
+	} else {
+	    $done{$_[0]} = 1;
+	    $_[1]++;
+	    $_[2] = $_[2] . "R";
+	    srch($_[0], $_[1], $_[2])
+	}
+    } else {
+	return;
+    }
+    if (u($current) == 0) {
+	if (exists($done{$_[0]})) {
+	    return;
+	} else {
+	    $done{$_[0]} = 1;
+	    $_[1]++;
+	    $_[2] = $_[2] . "U";
+	    srch($_[0], $_[1], $_[2])
+	}
+    } else {
+	return;
+    }
+    if (d($current) == 0) {
+	if (exists($done{$_[0]})) {
+	    return;
+	} else {
+	    $done{$_[0]} = 1;
+	    $_[1]++;
+	    $_[2] = $_[2] . "D";
+	    srch($_[0], $_[1], $_[2])
+	}
+    } else {
+	return;
+    }
+}
+
+sub test {
+    print "\n right\n";
+    $t = "01234=";
+    print "start: $t\n";
+    $ret = r($t);
+    print "ret = $ret, $t\n";
+    $ret = r($t);
+    print "ret = $ret, $t\n";
+    $ret = r($t);
+    print "ret = $ret, $t\n";
+
+    print "\n left\n";
+    $t = "=12340";
+    print "\nstart: $t\n";
+    $ret = l($t);
+    print "ret = $ret, $t\n";
+    $ret = l($t);
+    print "ret = $ret, $t\n";
+    $ret = l($t);
+    print "ret = $ret, $t\n";
+
+    $t = "=10234";
+    print "\nstart: $t\n";
+    $ret = l($t);
+    print "ret = $ret, $t\n";
+    $ret = l($t);
+    print "ret = $ret, $t\n";
+    $ret = l($t);
+    print "ret = $ret, $t\n";
+
+    print "\n up\n";
+    $t = "=12345670";
+    print "\nstart: $t\n";
+    $ret = u($t);
+    print "ret = $ret, $t\n";
+    $ret = u($t);
+    print "ret = $ret, $t\n";
+
+    $t = "123456780";
+    print "\nstart: $t\n";
+    $ret = u($t);
+    print "ret = $ret, $t\n";
+    $ret = u($t);
+    print "ret = $ret, $t\n";
+    $ret = u($t);
+    print "ret = $ret, $t\n";
+
+    print "\n down\n";
+    $t = "012345=67";
+    print "\nstart: $t\n";
+    $ret = d($t);
+    print "ret = $ret, $t\n";
+    $ret = d($t);
+    print "ret = $ret, $t\n";
+
+    $t = "012345678";
+    print "\nstart: $t\n";
+    $ret = d($t);
+    print "ret = $ret, $t\n";
+    $ret = d($t);
+    print "ret = $ret, $t\n";
+    $ret = d($t);
+    print "ret = $ret, $t\n";
+}
+
+##$done{$current} = 1;
+srch($current, $num, $path);
+
+#test();
